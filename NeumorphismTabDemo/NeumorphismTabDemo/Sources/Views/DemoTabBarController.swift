8748d0995186e90e3d9bf4de5835771505a669aa
touyou <fujiyou1121@gmail.com>
2020-01-12 23:59:25 +0900

    #1 #2 add full customize demo

--- NeumorphismTabDemo/NeumorphismTabDemo/Sources/Views/DemoTabBarController.swift
+++ NeumorphismTabDemo/NeumorphismTabDemo/Sources/Views/DemoTabBarController.swift
@@ -10,11 +10,23 @@ import UIKit
 import NeumorphismTab
 
 class DemoTabBarController: NeumorphismTabBarController {
+    override var tabHorizontalMargin: CGFloat {
+        get {
+            return 24
+        }
+    }
+
+    override var tabVerticalMargin: CGFloat {
+        get {
+            return 48
+        }
+    }
 
     override func setupView() {
         
         let color = UIColor(hex: "C1D2EB")
-        let home = NeumorphismTabBarItem(icon: UIImage(systemName: "house.fill")!, title: "Home")
+        let secondColor = UIColor(hex: "5E35B1")
+        let home = NeumorphismTabBarItem(icon: UIImage(systemName: "house.fill")!, title: "")
         let favorite = NeumorphismTabBarItem(icon: UIImage(systemName: "heart.fill")!, title: "")
 
         view.backgroundColor = color
@@ -27,11 +39,16 @@ class DemoTabBarController: NeumorphismTabBarController {
 
         let favoriteViewController: UIViewController = {
             let viewController = UIViewController()
-            viewController.view.backgroundColor = color
+            viewController.view.backgroundColor = secondColor
             return viewController
         }()
 
-        setTabBar(items: [home, favorite])
+        setTabBar(items: [home, favorite], height: 80, shadowOffset: 12, shadowBlur: 12)
         viewControllers = [homeViewController, favoriteViewController]
+    }
+
+    override func switchedTab(to toIndex: Int) {
+
+        backgroundColor = viewControllers?[toIndex].view.backgroundColor
     }
 }
